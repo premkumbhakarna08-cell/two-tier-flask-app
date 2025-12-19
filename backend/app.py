0ed0360c3a7f0af5a34806560f4b1d08da42dc00
premkumbhakarna08-cell-max <Premkumbhakarna08@gmail.com>
2025-12-19 18:09:40 +0530

    Setup two-tier Flask app

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/message')
 def message():
-    return jsonify({"Hello from Prem's Backend API - This message is sent from the backend service"})
+    return jsonify({"message": "Hello from Prem's Backend API - This message is sent from the backend service"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
